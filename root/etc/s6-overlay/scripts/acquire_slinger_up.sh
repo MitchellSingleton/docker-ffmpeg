99246ea72515ea32075d4a694857a2cc304a52fd
MitchellSingleton <MitchellSingleton@users.noreply.github.com>
2024-01-07 21:41:32 -0600

    Update and rename acquire_airconnect_up.sh to acquire_slinger_up.sh

--- root/etc/s6-overlay/scripts/acquire_slinger_up.sh
+++ root/etc/s6-overlay/scripts/acquire_slinger_up.sh
@@ -1,32 +1,15 @@
 #!/usr/bin/with-contenv bash
-echo "=== Starting acquire_airconnect_up.sh ==="
+echo "=== Starting acquire_slinger_up.sh ==="
 # This one-shot script is a dependacy and will run first.
 # It will check if the requested version has been downloaded and if not download it
 # It will check if the downloaded file has been unzipped and if not unzip
 # It will check if the desired binaries are copied into place with the correct permissions
 
+#filename to download
 var_filename=slingbox_server.py
 
-#download the current python script file
-https://raw.githubusercontent.com/GerryDazoo/Slinger/main/${var_filename}
-
-#test if CONFIG_VAR is zero length or not set, if so use default, if not use the passed variable
-if [ -z "${CONFIG_VAR}" ]; then
-    var_configname="/tmp"
-    echo "  ${var_path}/${var_filename} not found, downloading"
-    mkdir -p ${var_path}
-    # to allow saving download to path, change directory first.
-    #future investigate curl version and --output-dir flag
-    cd ${var_path}
-    curl -L -o ${var_filename} ${var_url}
-    cd /
-else
-   var_configname="${PATH_VAR}"
-fi
-
-#download the example files
-https://raw.githubusercontent.com/GerryDazoo/Slinger/main/config.ini
-https://raw.githubusercontent.com/GerryDazoo/Slinger/main/unified_config.ini
+#URL to download the current python script file
+var_url=https://raw.githubusercontent.com/GerryDazoo/Slinger/main/${var_filename}
 
 #test if PATH_VAR is zero length or not set, if so use default, if not use the passed variable
 if [ -z "${PATH_VAR}" ]; then
@@ -34,6 +17,36 @@ if [ -z "${PATH_VAR}" ]; then
 else
    var_path="${PATH_VAR}"
 fi
+
+mkdir -p ${var_path}
+
+#future check if file already exists so that downloading can be skipped - only works if download location is persistant
+echo " testing if ${var_path}/${var_filename} exists"
+if [ ! -f ${var_path}/${var_filename} ]; then
+    echo "  ${var_path}/${var_filename} not found, downloading"
+    # to allow saving download to path, change directory first.
+    #future investigate curl version and --output-dir flag
+    cd ${var_path}
+    curl -L -o ${var_filename} ${var_url}
+    cd /
+    #future add check for download success
+else
+    echo "  file exists"
+fi
+
+#filename to download
+var_config1=config.ini
+var_config2=unified_config.ini
+
+#URL to download the current python script file
+var_url1=https://raw.githubusercontent.com/GerryDazoo/Slinger/main/${var_filename}
+var_url1=https://raw.githubusercontent.com/GerryDazoo/Slinger/main/${var_filename}
+
+#download the example files
+https://raw.githubusercontent.com/GerryDazoo/Slinger/main/config.ini
+https://raw.githubusercontent.com/GerryDazoo/Slinger/main/unified_config.ini
+
+
 
 #future check if file already exists so that downloading can be skipped - only works if download location is persistant
 echo " testing if ${var_path}/${var_filename} exists"
